a4d2f90d7aa1e67251e0001d31b7d2f2a3d71c1c
Christiaan2 <christiaanreurslag@gmail.com>
2018-12-14 09:42:46 +0100

    Testing git

--- src/nii_viewer.py
+++ src/nii_viewer.py
@@ -2,14 +2,14 @@ import os
 import numpy as np
 import nibabel as nib
 from nilearn import plotting
-import cv2
+#import cv2
 
 def get_slope_inter(img):
     # Return the slope and y-intercept of the data.
     return img.dataobj.slope, img.dataobj.inter
 
 # Testing file
-file_path = r"data\Training_Batch1\media\nas\01_Datasets\CT\LITS\Training Batch 1\volume-1.nii"
+file_path = r"G:\AML - project\data\Training_Batch1\media\nas\01_Datasets\CT\LITS\Training Batch 1\volume-1.nii"
 
 # load the header to check what the linear transformation should be.
 img = nib.load(file_path)
@@ -34,6 +34,7 @@ pic[pic > 250] = 250
 pic = np.round(255 * ( pic + 250 ) / 500).astype('uint8', copy=False)
 print(np.min(pic), np.max(pic), np.std(pic), np.mean(pic), pic.shape)
 
+'''
 # Show a single slice with opencv
 while True:
     cv2.imshow('testing MRI', pic)
@@ -41,3 +42,4 @@ while True:
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+'''
