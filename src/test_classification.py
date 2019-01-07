7394b1367ed7eb1857e6a892c36a448c92a7dae3
Keulers <EKE@demcon.local>
2019-01-07 14:00:17 +0100

    test123

---
+++ src/test_classification.py
@@ -0,0 +1,43 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+
+print(len(train_labels))
+print(train_labels)
+print(test_images.shape)
+print(len(test_labels))
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(True)
+plt.show()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
